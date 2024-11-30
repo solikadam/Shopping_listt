d652694394d4185d90a5b4f602d635cd810ff0c3
Adam Solipiwko <solpiw14@gmail.com>
2024-11-30 20:23:47 +0100

    Dodaj pliki projektów.

---
+++ Shopping_list_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping_list_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Shopping_list_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping_list_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
